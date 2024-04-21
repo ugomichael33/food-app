948b5a7f049dc067843001b0af8b7f034e55bad4
michael onyekanma <michael@nebula.io>
2024-04-21 12:16:38 +0100

    bug fix

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: apiBaseUrl
 });
-
 
 export default axiosClient;
