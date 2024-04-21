1a6e2fa214c3e1f23f8f9175299210e8884c9e86
michael onyekanma <michael@nebula.io>
2024-04-21 12:31:22 +0100

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [vue()],
+    base: process.env.VITE_BASE_URL,
+  });
+}
